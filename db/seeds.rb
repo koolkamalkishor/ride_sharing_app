cc3ea49940dc4c1076593b17916a594482c074ab
ogomb <mbogolewis@gmail.com>
2018-08-21 10:49:51 +0300

    add admin flag to the user model and seeded the first  admin user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+unless User.exists?(email: "admin@sharemyride.com")
+  User.create!(email: "admin@sharemyride.com", password: "password", admin: true)
+end
+
+unless User.exists?(email: "viewer@sharemyride.com")
+  User.create!(email: "viewer@sharemyride.com", password: "password")
+end
+
+["Nairobi", "Kisumu"].each do |destination|
+  unless Ride.exists?(destination: destination)
+             Ride.create!(destination: destination, passengers: 4, checkout: "13:00")
+  end
+end
