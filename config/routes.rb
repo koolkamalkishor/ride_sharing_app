a0449aa106d57b41e2f5adc357082ace6167389d
ogomb <mbogolewis@gmail.com>
2018-08-21 11:29:00 +0300

    only admins can create or delete rides

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    root 'application#index'
+
+    resources :rides, only: [:new, :create, :destroy]
+  end
   devise_for :users
 
   root "rides#index"
 
-  resources :rides do
+  resources :rides, only: [:index, :show, :edit, :update] do
     resources :interests
   end
 end
